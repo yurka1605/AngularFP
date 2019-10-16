84fdea9572793207432f45f33f41c12407b0fb56
yurka1605 <litvinov.yuriy1996@gmail.com>
2019-10-16 17:02:35 +0400

    Design page 404

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AngularFP
 First project on angular
+Started Project:
+  1) npm install - for install dependencies
+  2) npm start - started this project on port 4242(localhost:4242)
--- src/app/components/404NotFound/404NotFound.component.ts
+++ src/app/components/404NotFound/404NotFound.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-auction-not-found',
   templateUrl: './404NotFound.component.html',
   styleUrls: ['./404NotFound.component.scss'],
 })
-// tslint:disable-next-line: class-name
-export class _404NotFoundComponent {
+
+export class _404NotFoundComponent implements OnInit {
+
+  @ViewChild('thirdDigit') thirdDigit: ElementRef;
+  @ViewChild('secondDigit') secondDigit: ElementRef;
+  @ViewChild('firstDigit') firstDigit: ElementRef;
+
   constructor() {
 
+  }
+
+  ngOnInit() {
+    const selector1 = this.firstDigit.nativeElement;
+    const selector2 = this.secondDigit.nativeElement;
+    const selector3 = this.thirdDigit.nativeElement;
+
+    this.timerNum(selector1, 4, 100);
+    this.timerNum(selector2, 0, 80);
+    this.timerNum(selector3, 4, 40);
+  }
+
+  timerNum(selector, textNum, inum) {
+    let i = 0;
+    const loop = setInterval(_ => {
+      if (i > inum) {
+          clearInterval(loop);
+          selector.textContent = textNum;
+      } else {
+          selector.textContent = this.randomNum();
+          i++;
+      }
+    }, 30);
+  }
+
+  randomNum() {
+    return Math.floor(Math.random() * 9) + 1;
   }
 }
